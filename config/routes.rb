348a77f23b4cf31fbdea02a86b7b06661eefb681
Matej Mandić <matejmobile516@gmail.com>
2021-03-23 13:07:22 +0100

    Remove devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :accounts
   resources :questions
   resources :answers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
