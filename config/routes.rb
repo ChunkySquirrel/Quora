e9b75075ad24c6adefdc39379374dbeadeefbd42
Ptatooo <kotoricdino52069@gmail.com>
2021-03-23 12:35:46 +0100

    Fix root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :questions
   resources :answers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   root 'public#index'
+   root 'questions#index'
 end
