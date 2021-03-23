3fd1b53fdd407979bf89b6c389e898da8b844563
Benjamin Hasanbasic <flarryzz0901@gmail.com>
2021-03-23 12:06:07 +0100

    devise fix

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,8 @@
 class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
