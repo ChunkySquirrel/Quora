8f507ee0ab4f4676201311310404250a08d9cf74
ChunkySquirrel <matejmobile516@gmail.com>
2021-03-30 13:09:09 +0200

    Add answers to questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,9 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: %i[ show edit update destroy ]
-
+  before_action :set_question
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:edit, :update, :create, :destroy]
+  before_action :owner?, only: %i[edit destroy]
   # GET /answers or /answers.json
   def index
     @answers = Answer.all
@@ -22,14 +25,16 @@ class AnswersController < ApplicationController
   # POST /answers or /answers.json
   def create
     @answer = Answer.new(answer_params)
+    @answer.user_id = current_user.id
+    @answer.question_id = @question.id
 
     respond_to do |format|
       if @answer.save
-        format.html { redirect_to @answer, notice: "Answer was successfully created." }
-        format.json { render :show, status: :created, location: @answer }
+        format.html { redirect_to @question, notice: "Answer was successfully created." }
+        format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -38,11 +43,11 @@ class AnswersController < ApplicationController
   def update
     respond_to do |format|
       if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: "Answer was successfully updated." }
+        format.html { redirect_to @question, notice: "Answer was successfully updated." }
         format.json { render :show, status: :ok, location: @answer }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -51,7 +56,7 @@ class AnswersController < ApplicationController
   def destroy
     @answer.destroy
     respond_to do |format|
-      format.html { redirect_to answers_url, notice: "Answer was successfully destroyed." }
+      format.html { redirect_to root_path, notice: "Answer was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -62,6 +67,9 @@ class AnswersController < ApplicationController
       @answer = Answer.find(params[:id])
     end
 
+    def set_question
+      @question = Question.find(params[:question_id])
+    end
     # Only allow a list of trusted parameters through.
     def answer_params
       params.require(:answer).permit(:user_id, :question_id, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ Rails.application.routes.draw do
     devise_scope :user do
       get '/users/sign_out' => 'devise/sessions#destroy'
     end
-  resources :questions
-  resources :answers
+  resources :questions do
+    resources :answers
+
+end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "questions#index"
