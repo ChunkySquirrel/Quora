4bf31541fe741644df276f31fcae6e2127cf99b2
Benjamin Hasanbasic <flarryzz0901@gmail.com>
2021-03-30 12:29:23 +0200

    admin fail

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :accounts
   resources :questions
   resources :answers
+  resources :admin
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root 'questions#index'
 end
