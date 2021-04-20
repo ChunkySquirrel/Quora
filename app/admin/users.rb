5bd9af9782400a0ff92277aa78739236fe242701
ChunkySquirrel <matejmobile516@gmail.com>
2021-04-20 12:46:00 +0200

    activeadmin user permits

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :name, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+ permit_params :email, :name, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmed_at
   #
   # or
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
