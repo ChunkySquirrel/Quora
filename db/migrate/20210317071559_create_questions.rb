b3c96f26be4317644e448c6fdeb3c198c8a8a135
ChunkySquirrel <matejmobile516@gmail.com>
2021-05-04 12:28:14 +0200

    Bilo sta

---
+++ db/migrate/20210317071559_create_questions.rb
@@ -0,0 +1,10 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :questions do |t|
+      t.integer :user_id
+      t.text :title
+
+      t.timestamps
+    end
+  end
+end
