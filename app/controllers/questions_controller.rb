d8597b03f9b5bcb8ec655e33493b654d6f76f9c3
Benjamin Hasanbasic <flarryzz0901@gmail.com>
2021-05-04 09:30:07 +0200

    Gas

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,85 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:edit, :update, :create, :destroy]
+  before_action :owner?, only: %i[edit destroy]
+
+
+
+  # GET /questions or /questions.json
+  def index
+    @questions = Question.all
+  end
+def import
+  Question.import(params[:file])
+  redirect_to '/admin/questions', notice: "Suscces my brotha"
+end
+ public :import
+  # GET /questions/1 or /questions/1.json
+  def show
+  end
+
+  # GET /questions/new
+  def new
+    @question = Question.new
+  end
+
+  # GET /questions/1/edit
+  def edit
+  end
+
+  # POST /questions or /questions.json
+  def create
+
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+
+    respond_to do |format|
+      if @question.save
+        format.html { redirect_to @question, notice: "Question was successfully created." }
+        format.json { render :show, status: :created, location: @question }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /questions/1 or /questions/1.json
+  def update
+    respond_to do |format|
+      if @question.update(question_params)
+        format.html { redirect_to @question, notice: "Question was successfully updated." }
+        format.json { render :show, status: :ok, location: @question }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /questions/1 or /questions/1.json
+  def destroy
+    @question.destroy
+    respond_to do |format|
+      format.html { redirect_to questions_url, notice: "Question was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def owner?
+    unless current_user == @question.user
+      redirect_back fallback_location: root_path, notice: 'User is not owner'
+    end
+  end
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def question_params
+      params.require(:question).permit(:user_id, :title)
+    end
+end
